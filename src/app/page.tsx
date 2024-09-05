5180243b6a6f317a9666f85f87bbfc917dc9721c
Daniel Krupnyi <daniel.krupnyi@gmail.com>
2024-09-05 11:40:34 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque at id,
+      quis distinctio totam esse quaerat adipisci! Nisi architecto aliquid,
+      molestias delectus distinctio dolorum reprehenderit, quibusdam ab autem
+      odio illo.
+    </div>
+  );
+}
